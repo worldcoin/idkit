930ebb556bc62e92ce5a1b6eaee09a7c59dba362
Gabe Cohen <gabe.cohen@toolsforhumanity.com>
2025-12-01 16:55:00 -0800

    kt publishing

--- kotlin/bindings/build.gradle.kts
+++ kotlin/bindings/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.24"
     `maven-publish`
+    signing
 }
 
 group = "com.worldcoin"
@@ -33,6 +34,8 @@ sourceSets {
 java {
     // ship sources for easier debugging/IDE usage
     withSourcesJar()
+    // align with external android lib publishing expectations
+    withJavadocJar()
 }
 
 publishing {
@@ -42,6 +45,28 @@ publishing {
             groupId = "com.worldcoin"
             artifactId = "idkit"
             version = project.version.toString()
+            pom {
+                name.set("IDKit Kotlin")
+                description.set("Kotlin bindings for IDKit backed by the Rust core")
+                url.set("https://github.com/worldcoin/idkit")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("worldcoin")
+                        name.set("Worldcoin")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/worldcoin/idkit.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/worldcoin/idkit.git")
+                    url.set("https://github.com/worldcoin/idkit")
+                }
+            }
         }
     }
     repositories {
@@ -53,5 +78,30 @@ publishing {
                 password = System.getenv("GITHUB_TOKEN") ?: ""
             }
         }
+        // Sonatype (Maven Central) kept for later enablement; requires OSSRH credentials and signing
+        // maven {
+        //     name = "sonatype"
+        //     url = uri(
+        //         if (version.toString().endsWith("SNAPSHOT"))
+        //             "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+        //         else
+        //             "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+        //     )
+        //     credentials {
+        //         username = System.getenv("OSSRH_USERNAME")
+        //         password = System.getenv("OSSRH_PASSWORD")
+        //     }
+        // }
+    }
+}
+
+signing {
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications["maven"])
+    } else {
+        logger.warn("Signing key not provided; artifacts will not be signed")
     }
 }
--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     // Gradle will resolve plugin versions from the included build scripts
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
 allprojects {
     repositories {
         mavenCentral()
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("OSSRH_USERNAME"))
+            password.set(System.getenv("OSSRH_PASSWORD"))
+        }
     }
 }
