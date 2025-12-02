dae66a5a0c798b8502ad276f365e4f935c97db7f
Gabe Cohen <gabe.cohen@toolsforhumanity.com>
2025-12-01 16:43:11 -0800

    fix kt publishing

--- kotlin/bindings/build.gradle.kts
+++ kotlin/bindings/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.24"
+    `maven-publish`
 }
 
 group = "com.worldcoin"
@@ -26,5 +27,31 @@ sourceSets {
     named("main") {
         java.srcDir("src/main/kotlin")
         resources.srcDir("src/main/resources")
+    }
+}
+
+java {
+    // ship sources for easier debugging/IDE usage
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = "com.worldcoin"
+            artifactId = "idkit"
+            version = project.version.toString()
+        }
+    }
+    repositories {
+        maven {
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/worldcoin/idkit")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: ""
+                password = System.getenv("GITHUB_TOKEN") ?: ""
+            }
+        }
     }
 }
