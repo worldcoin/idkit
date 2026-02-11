db83c30f0a63ba9a6e2b0ce709132d48b540768b
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2026-02-11 18:17:06 +0200

    chore(js): reduce test env deps

--- js/packages/react/vitest.config.ts
+++ js/packages/react/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
     exclude: ["**/node_modules/**", "**/dist/**"],
   },
