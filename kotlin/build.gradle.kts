e5dbaa275172a447f46d777587615f40bcb33614
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2026-02-25 21:14:47 -0800

    Adopt Android AAR publishing for idk

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.library") version "8.7.3" apply false
+    kotlin("android") version "1.9.24" apply false
+}
+
 allprojects {
     repositories {
         mavenCentral()
