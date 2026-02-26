2ff0044c597eeaba4340146d2e3ae0f8782bd8e1
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2026-02-26 13:54:30 -0800

    Clarify Android CI confidence

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -2,9 +2,3 @@ plugins {
     id("com.android.library") version "8.7.3" apply false
     kotlin("android") version "1.9.24" apply false
 }
-
-allprojects {
-    repositories {
-        mavenCentral()
-    }
-}
