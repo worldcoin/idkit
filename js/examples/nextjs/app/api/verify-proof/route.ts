95f0a280ea3dd6e553bb495f0b91f62d47f1d96f
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2026-03-08 23:36:02 -0700

    default signal_hash if it's not returned

--- js/examples/nextjs/app/api/verify-proof/route.ts
+++ js/examples/nextjs/app/api/verify-proof/route.ts
@@ -1,3 +1,4 @@
+import { IDKitResult } from "@worldcoin/idkit";
 import { NextResponse } from "next/server";
 
 // export const runtime = "nodejs";
@@ -6,11 +7,39 @@ export async function POST(request: Request): Promise<Response> {
   try {
     const body = (await request.json()) as {
       rp_id?: string;
-      devPortalPayload?: unknown;
+      devPortalPayload?: IDKitResult;
     };
 
     const baseUrl =
       process.env.DEV_PORTAL_BASE_URL?.trim() || "https://developer.world.org";
+
+    const devPortalPayloadWithDefaultSignal = {
+      ...body.devPortalPayload,
+      responses:
+        body.devPortalPayload?.responses.map((r) => {
+          let signal_hash = r.signal_hash;
+          // 3.0 proof and signal doens't exist
+          if (
+            !signal_hash &&
+            body.devPortalPayload?.protocol_version === "3.0"
+          ) {
+            signal_hash =
+              "0x00c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a4";
+          }
+          // 4.0 proof and signal doesn't exist
+          else if (
+            !signal_hash &&
+            body.devPortalPayload?.protocol_version === "4.0"
+          ) {
+            signal_hash = "0x0";
+          }
+
+          return {
+            ...r,
+            signal_hash,
+          };
+        }) || [],
+    };
 
     const response = await fetch(`${baseUrl}/api/v4/verify/${body.rp_id}`, {
       method: "POST",
